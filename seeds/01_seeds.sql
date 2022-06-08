39ac653f63d9cd158f6e9eefe7fbc2ab97a56e46
Raiza De Guzman <santos_dg7@hotmail.com>
2022-06-08 14:17:02 -0700

    Add three records of fake data into properties table.

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -2,3 +2,11 @@ INSERT INTO users (name, email, password)
 VALUES ('Clarke Kent', 'ckent@yahoo.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
 ('Lana Reyes', 'lanareyes@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
 ('Diana Prince', 'dianaprince@hotmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u');
+
+INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active)
+
+VALUES (1, Restful abode, description, https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg?auto=compress&cs=tinysrgb&h=350, https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg, 900, 4, 5, 7, Canada, 536 Monkeylane, Calgary, Alberta, 83680, true),
+
+VALUES (1, Introverts den, description, https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350, https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg, 630, 3, 4, 5, Canada, 657 Flower Road, Winnipeg, Manitoba, 73619, true),
+
+VALUES (2, Extroverts oasis, description, https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg?auto=compress&cs=tinysrgb&h=350, https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg, 750, 2, 3, 5, Canada, 871 Firefly Road, Chilliwack, British Columbia, 71820, true);
