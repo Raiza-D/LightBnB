544334968316365e6320e243f2cbe97ec671cdac
Raiza De Guzman <santos_dg7@hotmail.com>
2022-06-08 14:03:23 -0700

    Create new file to store data for each of tables. Add three records for users table.

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (name, email, password)
+VALUES ('Clarke Kent', 'ckent@yahoo.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
+('Lana Reyes', 'lanareyes@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
+('Diana Prince', 'dianaprince@hotmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u');
