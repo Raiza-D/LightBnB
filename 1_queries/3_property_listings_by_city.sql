4fa3561113632de86d218953d368a2dfae01c47b
Raiza De Guzman <santos_dg7@hotmail.com>
2022-06-08 16:29:28 -0700

    Edit query to match toggled answer on Compass.

--- 1_queries/3_property_listings_by_city.sql
+++ 1_queries/3_property_listings_by_city.sql
@@ -1,10 +1,11 @@
 -- Retrieve Vancouver properties with average rating of 4 stars.
 -- Results should show property's id, title, cost per night, and its average rating
 
-SELECT properties.id, title, cost_per_night, AVG(rating) as average_rating
+SELECT properties.id, title, cost_per_night, AVG(property_reviews.rating) as average_rating
 FROM properties
-JOIN property_reviews ON properties.id = property_id
-WHERE city = 'Vancouver' AND property_reviews.rating >= 4
+LEFT JOIN property_reviews ON properties.id = property_id
+WHERE city LIKE '%ancouv%'
 GROUP BY properties.id
+HAVING AVG(property_reviews.rating) >= 4
 ORDER BY cost_per_night
 LIMIT 10;
