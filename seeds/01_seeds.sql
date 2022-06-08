2a4cc48343bbf3d2b7848600e93092d8cc0c6404
Raiza De Guzman <santos_dg7@hotmail.com>
2022-06-08 14:39:44 -0700

    Add three records of fake data for property_reviews table. Made sure to match info from records on reservations table e.g. guest and property id's.

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -12,3 +12,8 @@ INSERT INTO reservations (start_date, end_date, property_id, guest_id)
 VALUES ('2018-09-11', '2018-09-26', 2, 3),
 ('2019-01-04', '2019-02-01', 2, 2),
 ('2021-10-01', '2021-10-14', 1, 1);
+
+INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message)
+VALUES (3, 2, 1, 3, 'messages'),
+(2, 2, 2, 4, 'messages'),
+(1, 1, 3, 5, 'messages');
