3abd51a366f29c726d10caf78b04625aa18d7a52
Raiza De Guzman <santos_dg7@hotmail.com>
2022-06-08 16:11:43 -0700

    Add comments above query.

--- 1_queries/3_property_listings_by_city.sql
+++ 1_queries/3_property_listings_by_city.sql
@@ -1,3 +1,6 @@
+-- Retrieve Vancouver properties with average rating of 4 stars.
+-- Results should show property's id, title, cost per night, and its average rating
+
 SELECT properties.id, title, cost_per_night, AVG(rating) as average_rating
 FROM properties
 JOIN property_reviews ON properties.id = property_id
