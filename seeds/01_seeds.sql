293d6d17b98044d3970705f619d39e9b616a3a47
Raiza De Guzman <santos_dg7@hotmail.com>
2022-06-08 14:34:58 -0700

    Add three records of fake data for reservations table.

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -7,3 +7,8 @@ INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover
 VALUES (1, 'Restful abode', 'description', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg', 900, 4, 5, 7, 'Canada', '536 Monkeylane', 'Calgary', 'Alberta', 83680, true),
 (1, 'Introverts den', 'description', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg', 630, 3, 4, 5, 'Canada', '657 Flower Road', 'Winnipeg', 'Manitoba', 73619, true),
 (2, 'Extroverts oasis', 'description', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg', 750, 2, 3, 5, 'Canada', '871 Firefly Road', 'Chilliwack', 'British Columbia', 71820, true);
+
+INSERT INTO reservations (start_date, end_date, property_id, guest_id)
+VALUES ('2018-09-11', '2018-09-26', 2, 3),
+('2019-01-04', '2019-02-01', 2, 2),
+('2021-10-01', '2021-10-14', 1, 1);
