52f3222f2cf91234991732c051863d04f5d95e2a
Raiza De Guzman <santos_dg7@hotmail.com>
2022-06-08 16:08:32 -0700

    Create new file for query to retrieve avg rating for a property in a given city and where rating is greater than or equal to 4 stars.

---
+++ 1_queries/3_property_listings_by_city.sql
@@ -0,0 +1,7 @@
+SELECT properties.id, title, cost_per_night, AVG(rating) as average_rating
+FROM properties
+JOIN property_reviews ON properties.id = property_id
+WHERE city = 'Vancouver' AND property_reviews.rating >= 4
+GROUP BY properties.id
+ORDER BY cost_per_night
+LIMIT 10;
