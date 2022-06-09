31f2e90ee6356fd014fdd5db666560e6b7313c74
Raiza De Guzman <santos_dg7@hotmail.com>
2022-06-08 18:25:36 -0700

    Edit query to closely match Compass' toggled solution. Add Limit 10, forgot about it.

--- 1_queries/5_all_my_reservations.sql
+++ 1_queries/5_all_my_reservations.sql
@@ -1,8 +1,9 @@
 -- Retrieve all the reservations for a single user
 SELECT reservations.id, properties.title, properties.cost_per_night, reservations.start_date, AVG(property_reviews.rating) as average_rating
 FROM reservations
-JOIN properties ON properties.id = property_id
-JOIN property_reviews ON properties.id = property_reviews.property_id
+JOIN properties ON reservations.property_id = properties.id
+JOIN property_reviews ON property_reviews.property_id = properties.id
 WHERE reservations.guest_id = 1
-GROUP BY reservations.id, properties.title, properties.cost_per_night
-ORDER BY reservations.start_date ASC;
+GROUP BY properties.id, reservations.id
+ORDER BY reservations.start_date ASC
+LIMIT 10;
