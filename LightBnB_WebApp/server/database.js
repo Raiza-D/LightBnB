302d611565464e622d30528403491a9d4d8d1fda
Raiza De Guzman <santos_dg7@hotmail.com>
2022-06-13 18:00:29 -0700

    Remove console log within if statement of getUserWithId function.

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -44,7 +44,6 @@ const getUserWithId = function(id) {
   .query(`SELECT id, name, email, password FROM users WHERE id = $1`, [id])
   .then((result) => {
     if (result.rows.length > 0) {
-      console.log(result.rows);
       return result.rows[0];
     } else {
       return null;
