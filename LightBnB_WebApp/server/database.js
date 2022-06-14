a4cf866012fba45a287f00029580a2dd902a1acf
Raiza De Guzman <santos_dg7@hotmail.com>
2022-06-14 13:55:36 -0700

    Refactor getAllReservations to show reservations for a user. Values on page showing up undefined. Have to fix. Correct Line 121 second param for query from limit to 10. 'Limit' doesn't work.

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -80,7 +80,31 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+  .query(`SELECT reservations.*, properties.*, AVG(rating) as average_rating
+    FROM reservations
+    JOIN properties ON reservations.property_id = properties.id
+    JOIN property_reviews ON properties.id = property_reviews.property_id
+    WHERE reservations.guest_id = $1
+    GROUP BY properties.id, reservations.id
+    ORDER BY reservations.start_date
+    LIMIT $2;`, [guest_id, 10])
+
+  // return pool.query(queryString, values)
+  .then((result) => {
+    console.log(result.rows[0]);
+    return result.rows[0];
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+/* All values showing up as undefined. Dates wrong. Rating wrong. Cost wrong.
+Results only show 7 for guest_id 743 when they have 11 reservations total.
+console.log is wrong on Terminal*/
+
+/* Previous code:
+  return getAllProperties(null, 2); */
+
 }
 exports.getAllReservations = getAllReservations;
 
@@ -94,7 +118,7 @@ exports.getAllReservations = getAllReservations;
  */
 const getAllProperties = function(options, limit = 10) {
   return pool
-    .query(`SELECT * FROM properties LIMIT $1`, [limit])
+    .query(`SELECT * FROM properties LIMIT $1;`, [10])
     .then((result) => {
       // console.log(result.rows);
       // properties = result.rows;
