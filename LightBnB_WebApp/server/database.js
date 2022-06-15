0a216b7a0d026ea1587743b35adc02dee438bc92
Raiza De Guzman <santos_dg7@hotmail.com>
2022-06-14 22:27:00 -0700

    Update Line 92 allReservationsValues array's second element to say just 'limit' instead of 10. Misread Compass instructions.

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -89,7 +89,7 @@ const getAllReservations = function(guest_id, limit = 10) {
     ORDER BY reservations.start_date
     LIMIT $2;`;
 
-  const allReservationsValues = [guest_id, 10];
+  const allReservationsValues = [guest_id, limit];
 
   return pool.query(queryAllReservations, allReservationsValues)
   .then((result) => {
