6956e5b5bed013e3930515a7c46e1546397e3ba3
Raiza De Guzman <santos_dg7@hotmail.com>
2022-06-16 17:02:13 -0700

    Stretch Activity: LighthouseBnB More Tables. Create new file. Contains CREATE TABLE queries to create rates and guest reviews tables.

---
+++ migrations/02_rates_guest_reviews.sql
@@ -0,0 +1,21 @@
+DROP TABLE IF EXISTS rates CASCADE;
+DROP TABLE IF EXISTS guest_reviews CASCADE;
+
+-- Create rates table, its columns, and their attributes
+CREATE TABLE rates (
+  id SERIAL PRIMARY KEY NOT NULL,
+  start_date DATE NOT NULL,
+  end_date DATE NOT NULL,
+  cost_per_night INTEGER NOT NULL,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE
+);
+
+-- Create guest_reviews table, its columns, and their attributes
+CREATE TABLE guest_reviews (
+  id SERIAL PRIMARY KEY NOT NULL,
+  owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
+  rating SMALLINT NOT NULL DEFAULT 0,
+  message TEXT
+);
